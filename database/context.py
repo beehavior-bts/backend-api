4753b8090441d51920da6055f4f3307749f88813
Estéban Ristich <esteban.ristich@protonmail.com>
2022-02-18 19:24:44 +0100

    Add database context connector (context.py)

---
+++ database/context.py
@@ -0,0 +1,26 @@
+import psycopg2
+# from utils.core import api_message
+from core.config import AppState
+
+class PGDatabase:
+
+    def __init__(self) -> None:
+        self._conn = None
+
+    def connect(self):
+        if self._conn is None:
+            try:
+                self._conn = psycopg2.connect(
+                    host=AppState.Database.HOST,
+                    user=AppState.Database.USER,
+                    password=AppState.Database.PASS,
+                    port=AppState.Database.PORT,
+                    dbname=AppState.Database.NAME
+                )
+                # api_message("i", f'encoding : {self._conn.encoding}')
+            except psycopg2.DatabaseError as e:
+                # api_message("d", f'Failed to connect to PostgreSQL database, error : {e}')
+                exit(0)
+
+        # api_message("d", f'Success to connect to PostgreSQL database')
+        return self._conn
