a4e0947088190994a597bb0d11fbdbfcd6871455
Estéban Ristich <esteban.ristich@protonmail.com>
2022-02-21 00:58:15 +0100

    Add accept media type json middleware

--- routes/middlewares.py
+++ routes/middlewares.py
@@ -10,3 +10,17 @@ class DebugMiddleware(object):
 
     def process_response(self, req, resp, resource, req_succeeded):
         api_message('d', f'[MIDDLEWARE][{req.path}] => {resp.media}')
+
+class AcceptJSONMiddleware:
+
+    def process_request(self, req, resp):
+
+        if not req.client_accepts_json:
+            raise falcon.HTTPNotAcceptable('This API only supports responses encoded as JSON.')
+            
+        if req.content_type == falcon.MEDIA_JSON:
+            return
+
+    def process_response(self, req, resp, resource, req_succeeded):
+        resp.set_header('Accept', falcon.MEDIA_JSON)
+        resp.set_header('X-Api-Version', ".".join(map(str, AppState.VERSION)))
