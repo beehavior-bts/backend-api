48cad2e9bb5b6bd30c490b70b7462d19a46daf76
Estéban Ristich <esteban.ristich@protonmail.com>
2022-02-20 17:06:11 +0100

    Remove wsgi loader in app.py

--- app.py
+++ app.py
@@ -10,12 +10,3 @@ db.connect()
 api = application = falcon.App(middleware=[])
 
 api.add_route('/api/auth/login', LoginController())
-
-if platform.system() == "Linux":
-    import bjoern
-    bjoern.run(api, host=AppState.HOST, port=AppState.PORT, reuse_port=True)
-else:
-    # import uvicorn
-    # uvicorn.run(api, host=AppState.HOST, port=AppState.PORT, log_level=AppState.LOGGING_LEVEL)
-    import waitress
-    waitress.serve(api, host=AppState.HOST, port=AppState.PORT)
