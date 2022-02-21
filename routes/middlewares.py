4612c3fc0327af081f8a607901b1712d0ef53bd8
Estéban Ristich <esteban.ristich@protonmail.com>
2022-02-21 01:01:08 +0100

    Add database middleware reconnexion

--- routes/middlewares.py
+++ routes/middlewares.py
@@ -1,6 +1,7 @@
 import falcon
 from core.utils import api_message
 from core.config import AppState
+from database.context import PGDatabase
 
 class DebugMiddleware(object):
 
@@ -24,3 +25,15 @@ class AcceptJSONMiddleware:
     def process_response(self, req, resp, resource, req_succeeded):
         resp.set_header('Accept', falcon.MEDIA_JSON)
         resp.set_header('X-Api-Version', ".".join(map(str, AppState.VERSION)))
+
+
+class DatabaseConnectMiddleware():
+    
+    def process_resource(self, req, resp, resource, params):
+        if resource is not None:
+            AppState.Database.CONN.close()
+            PGDatabase().connect_to_api()
+
+    def process_response(self, req, resp, resource, req_succeeded):
+        if AppState.Database.CONN:
+            AppState.Database.CONN.close()
