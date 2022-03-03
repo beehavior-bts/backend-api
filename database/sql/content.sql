c721967373c08bbaeb1fb6266188606336c862eb
Estéban Ristich <esteban.ristich@protonmail.com>
2022-03-03 10:47:30 +0100

    Clean for php edition laul -_-

--- database/sql/content.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO accounts (email, username, password, is_admin)
-    VALUES 
-('admin@beehavior.com', 'Admin', '$argon2id$v=19$m=102400,t=2,p=8$PmADmWUrgIqQux7x2ut1jg$+CYmq5abt7gc1IEkQteLeg', TRUE);
