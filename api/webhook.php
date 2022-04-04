b13b86ba2b8b304bc017ce5dd9e858062c4383bd
Estéban Ristich <esteban.ristich@protonmail.com>
2022-04-05 00:28:58 +0200

    Active webhook for only post method

--- api/webhook.php
+++ api/webhook.php
@@ -4,30 +4,34 @@ require '../database/model.php';
 
 header("Content-Type: application/json; charset=UTF-8");
 // $req = json_decode($_POST["laul"], false);
-$content = json_decode(trim(file_get_contents("php://input")), true);
 
-$end_device_id = strtoupper(explode("-", $content["end_device_ids"]["device_id"])[1]);
-$received_at = $content["received_at"];
-$payload = $content["uplink_message"]["frm_payload"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $content = json_decode(trim(file_get_contents("php://input")), true);
 
-// "humidity|temperature|mass"
-$dec_payload = base64_decode($payload);
-$metrics = explode("|", $dec_payload);
-$humidity = intval($metrics[0]);
-$temperature = floatval($metrics[1]);
-$mass = floatval($metrics[2]);
+    $end_device_id = strtoupper(explode("-", $content["end_device_ids"]["device_id"])[1]);
+    $received_at = $content["received_at"];
+    $payload = $content["uplink_message"]["frm_payload"];
 
-$dbmetric = new Metric();
-$dbmetric->insert($end_device_id, $humidity, $temperature, $mass);
+    // "humidity|temperature|mass"
+    $dec_payload = base64_decode($payload);
+    $metrics = explode("|", $dec_payload);
+    $humidity = intval($metrics[0]);
+    $temperature = floatval($metrics[1]);
+    $mass = floatval($metrics[2]);
 
-// $dbaccount->insert("ristich.esteban.lgm@gmail.com", "Estéban");
-/*
-$dbaccount = new Account();
-*/
+    $dbmetric = new Metric();
+    $dbmetric->insert($end_device_id, $humidity, $temperature, $mass);
 
-$resp = json_encode(array(
-    "title" => "SAVED",
-    "description" => "Metric has been saved in database",
-));
-echo $resp;
+    // $dbaccount->insert("ristich.esteban.lgm@gmail.com", "Estéban");
+    /*
+    $dbaccount = new Account();
+    */
+
+    $resp = json_encode(array(
+        "title" => "SAVED",
+        "description" => "Metric has been saved in database",
+    ));
+    echo $resp;
+}
+
 ?>
